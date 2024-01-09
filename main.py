6696af8770c3faf4d85d87ef5e02ac8b0a679ee3
PanayGeo <145594864+PanayGeo@users.noreply.github.com>
2024-01-09 11:25:42 -0500

    Update and rename no.py to main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import time, random
+
+while desire == True:
+
+    wish=input('Would you like to play again? (y)es/(n)o\n')
+    if y in wish.lower():
+        pass
+    elif n in wish.lower():
+        break
+    else:
+        print('*knocks you out*')
+        pass        
