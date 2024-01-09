2625a669c4ac2ab667b5528116610a019aa3bbb4
PanayGeo <145594864+PanayGeo@users.noreply.github.com>
2024-01-09 11:27:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time, random
-
+desire = True
 while desire == True:
 
     wish=input('Would you like to play again? (y)es/(n)o\n')
-    if y in wish.lower():
+    if 'y' in wish.lower():
         pass
-    elif n in wish.lower():
+    elif 'n' in wish.lower():
         break
     else:
         print('*knocks you out*')
