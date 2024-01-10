f9920d664dbc535cfa24d4b89d7001de88a6787f
PanayGeo <145594864+PanayGeo@users.noreply.github.com>
2024-01-10 11:00:46 -0500

    Update main.py
    mhm

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time, random
 desire = True
 while desire == True:
-
+    print('Hello Welcome to Roshambo')
+    time.sleep(1)
+    print('otherwose known as Rock, Paper, Sciccors')
+    time.sleep(3)
     wish=input('Would you like to play again? (y)es/(n)o\n')
     if 'y' in wish.lower():
         pass
