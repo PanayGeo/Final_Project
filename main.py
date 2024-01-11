50b99c42e38c74749ade32cfd03af6b1b68503de
PanayGeo <145594864+PanayGeo@users.noreply.github.com>
2024-01-11 11:14:44 -0500

    Update main.py
    Pemdas nakeyjakey

--- main.py
+++ main.py
@@ -13,22 +13,39 @@ while desire == True:
         try:
             x=1
             user_choice=input('Choose either {Rock}, {Paper}, or {Scissors}\n')
-            cpu_choice=random.choice(['rock','paper','scissors'])
+            print('\n')
+            choice_list = ['rock','paper','scissors']
+            cpu_choice = random.choice(choice_list)
             user_draw = f'You cast {user_choice}'
             cpu_draw = f'Computer cast {cpu_choice}'
-            cpu_value = cpu_choice.index
-            if 'rock' in user_choice.lower
+            cpu_value = int(choice_list.index(cpu_choice))+1
+            if 'rock' in user_choice.lower():
                 user_value = 1
-            elif 'paper' in user_choice.lower
+                break
+            elif 'paper' in user_choice.lower():
                 user_value = 2
-            elif 'scissors' in user_choice.lower
+                break
+            elif 'scissors' in user_choice.lower():
                 user_value = 3   
-            if 
-                break      
-            elif 
+                break
+            else:
+                raise ValueError
         except ValueError:
             print('that is not a valid input')
-            pass         
+            pass      
+    for item in choice_list:
+        print(item.title())
+        time.sleep(1)
+    print(user_draw)  
+    print(cpu_draw)
+    if cpu_value>user_value:
+        print('You Lost D:')
+    elif user_value>cpu_value:
+        print('You WIN!!!')
+    elif user_value==cpu_value:
+        print('Tie Game')
+    else: 
+        print('If the code broke I\'m not even suprised\n I have no idea what\'s going on anymore')
     time.sleep(x)
     wish=input('Would you like to play again? (y)es/(n)o\n')
     if 'y' in wish.lower():
